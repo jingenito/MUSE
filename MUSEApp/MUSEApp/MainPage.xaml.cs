dfb4eaf388bbb7f0f489438c73a3e573119f36ce
jingenito <jingenito1530@gmail.com>
2020-06-07 16:04:10 -0400

    app can now make a request to the server

--- MUSEApp/MUSEApp/MainPage.xaml.cs
+++ MUSEApp/MUSEApp/MainPage.xaml.cs
@@ -6,11 +6,18 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
+using MUSECommonLibrary;
+
 namespace MUSEApp
 {
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        List<string> constants = new List<string>(new string[] { "pi", "e" });
+
+        const double euler = 2.7182818284590452353602874713527;
+        const double pi = 3.1415926535897932384626433832794;
+
         public MainPage()
         {
             InitializeComponent();
@@ -24,9 +31,65 @@ namespace MUSEApp
             await DisplayAlert("All constants are case sensitive", s.ToString(), "Ok");
         }
 
-        private void buttSubmit_Clicked(object sender, EventArgs e)
+        private async void buttSubmit_Clicked(object sender, EventArgs e)
         {
+            double num;
+            var num_text = txtNumber.Text.Trim();
+            if (num_text == "pi")
+            {
+                num = pi;
+            }
+            else if (num_text == "e")
+            {
+                num = euler;
+            }
+            else if (!double.TryParse(num_text, out num))
+            {
+                await DisplayAlert("Invalid", "There was an error parsing the number", "Ok");
+                return;
+            }
 
+            int count;
+            if (!int.TryParse(txtIterations.Text, out count))
+            {
+                await DisplayAlert("Invalid", "There was an error parsing the iterations", "Ok");
+                return;
+            }
+
+            ContinuedFractionResponse resp = await WebServiceCom.POSTContinuedFractionRequest(num, count);
+            if (resp == null)
+            {
+                await DisplayAlert("Server Error", "There was an issue communicating the server", "Ok");
+                return;
+            }
+            else if (!string.IsNullOrWhiteSpace(resp.ServerResponse))
+            {
+                await DisplayAlert("Bad Request", resp.ServerResponse, "Ok");
+                return;
+            }
+            else
+            {
+                txtOutput.Text = convertEnumerableToString(resp.qSequence);
+            }
+        }
+
+        private string convertEnumerableToString(IEnumerable<int> qSequence)
+        {
+            var s = new StringBuilder();
+            foreach (var q in qSequence)
+            {
+                if (q > 0)
+                {
+                    s.AppendFormat("{0}, ", q);
+                }
+            }
+
+            if (s.Length > 2)
+            {
+                s.Length -= 2;
+            }
+
+            return s.ToString();
         }
     }
 }
