7f6e9f328d4bed6972f4de05a8ae4c5e94c062ee
Malekith <jingenito@vcssoftware.com>
2020-06-30 19:15:18 -0400

    work done on laptop

--- CPP/CPPHeader.h
+++ CPP/CPPHeader.h
@@ -13,21 +13,6 @@
 using json = nlohmann::json;
 
 const int MAXCOUNT = 100;
-const double pi = 3.14159265358979323846;
-const double e = 2.7182818284590452353602874713527;
-const double phi = 1.61803398874989484820458683436;
-
-extern long* continuedFractionExpansion(double, size_t&, size_t&);
-extern void printArray(long*, size_t);
-extern void printConvergences(long*, size_t);
-extern double tryParseNumFromArgs(std::string);
-extern double tryParseMathFromArgs(std::string);
-
-inline bool is_number(const std::string& s)
-{
-    return !s.empty() && find_if(s.begin(),
-        s.end(), [](unsigned char c) { return !isdigit(c); }) == s.end();
-}
 
 json convertQSequenceToJSON(long*, size_t, size_t);
 
--- CPP/main.cpp
+++ CPP/main.cpp
@@ -32,10 +32,10 @@ int main(int argc, char** argv)
 	}
 
 	std::string temp_num = *(argv + 1);
-	num = tryParseNumFromArgs(temp_num);
+	num = CPPMathLibrary::TryParseNumFromArgs(temp_num);
 	if (num == 0.0) {
-		num = tryParseMathFromArgs(temp_num);
-		if (num == 0.0 && is_number(temp_num)) {
+		num = CPPMathLibrary::TryParseMathFromArgs(temp_num);
+		if (num == 0.0 && CPPMathLibrary::IsNumeric(temp_num)) {
 			num = stod(temp_num);
 		}
 	}
@@ -47,7 +47,7 @@ int main(int argc, char** argv)
 	}
 
 	temp_num = *(argv + 2);
-	if (!is_number(temp_num)) {
+	if (!CPPMathLibrary::IsNumeric(temp_num)) {
 		std::cout << "Count is not a number." << std::endl;
 		return 0;
 	}
@@ -60,11 +60,11 @@ int main(int argc, char** argv)
 	filename = *(argv + 3);
 
 	size_t op_count = 0;
-	long* qs = continuedFractionExpansion(num, count, op_count);
+	long* qs = CPPMathLibrary::ContinuedFraction::ContinuedFractionExpansion(num, count, op_count);
 	if (!server_mode) {
 		std::cout << "Operation Count: " << op_count << std::endl;
-		printArray(qs, count);
-		printConvergences(qs, count);
+		/*ContinuedFraction::PrintArray(qs, count);
+		ContinuedFraction::PrintConvergences(qs, count);*/
 		std::cout << "Writing array to " << filename << std::endl;
 	}
 
@@ -73,62 +73,6 @@ int main(int argc, char** argv)
 	out << j << std::endl;
 
 	return 0;
-}
-
-double tryParseNumFromArgs(std::string s) {
-	if (s == "pi") {
-		return pi;
-	}
-	else if (s == "e") {
-		return e;
-	}
-	else if (s == "phi") {
-		return phi;
-	}
-	return 0.0;
-}
-
-double tryParseMathFromArgs(std::string s) {
-	size_t pos1 = s.find("(");
-	size_t pos2 = s.find(")");
-	if (pos1 == std::string::npos || pos2 == std::string::npos || pos2 < pos1) { return 0.0; }
-
-	size_t dist = pos2 - pos1 - 1; //length of the substd::string needed to extract the number
-	if (dist == 0) { return 0.0; }
-
-	std::string temp_num = s.substr(pos1 + 1, dist); //extract the number from the parenthesis
-	double num = tryParseNumFromArgs(temp_num);
-	if (num == 0.0 && is_number(temp_num)) {
-		num = stod(temp_num);
-	}
-
-	std::string m = s.substr(0, pos1); //extract the mode
-	if (m == "sqrt" && num >= 0) {
-		return sqrt(num);
-	}
-	else {
-		size_t und = s.find("_");
-		if (und == std::string::npos) {
-			//still need to check for log
-			if (m == "log" && num > 0) {
-				return log(num);
-			}
-		}
-
-		m = s.substr(0, und);
-		if (m == "log" && num > 0) {
-			dist = pos1 - und - 1;
-			temp_num = s.substr(und + 1, dist);
-			if (is_number(temp_num)) {
-				size_t ind = stoi(temp_num);
-				if (ind > 0) {
-					return log(num) / log(ind);
-				}
-			}
-		}
-	}
-
-	return 0.0;
 }
 
 json convertQSequenceToJSON(long* qs, size_t count, size_t op_count) {
