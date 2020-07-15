fa148db7a22d7fd20d0da85a0ff521777aad79a8
Joe <jingenito@vcssoftware.com>
2020-07-15 00:47:09 -0400

    added more unmanged test ports

--- TestAPP/CPPMathLibTest.cs
+++ TestAPP/CPPMathLibTest.cs
@@ -21,14 +21,20 @@ namespace TestAPP
         private extern static IntPtr _continuedFraction(double gamma, ref int count);
 
         [DllImport(CPPDLLPath, EntryPoint = "CPPMathLibrary_ManagedPort_ReleaseMemoryIntArr", CallingConvention = CallingConvention.Cdecl)]
-        private extern static int ReleaseMemory(IntPtr arr);
+        private extern static int ReleaseMemoryIntPtr(IntPtr arr);
+
+        [DllImport(CPPDLLPath, EntryPoint = "CPPMathLibrary_ManagedPort_ReleaseMemoryIntPtrPtr", CallingConvention = CallingConvention.Cdecl)]
+        private extern unsafe static int ReleaseMemoryIntPtrPtr(int** arr, int n);
+
+        [DllImport(CPPDLLPath, EntryPoint = "CPPMathLibrary_ManagedPort_ReleaseMemoryDoubleArr", CallingConvention = CallingConvention.Cdecl)]
+        private extern unsafe static int ReleaseMemoryDoublePtrPtr(double** arr, int n);
 
         public static int[] ContinuedFraction(double gamma, ref int count)
         {
             IntPtr qs = _continuedFraction(gamma, ref count);
             int[] result = new int[count];
             Marshal.Copy(qs, result, 0, count);
-            ReleaseMemory(qs);
+            ReleaseMemoryIntPtr(qs);
             return result;
         }
 
@@ -41,19 +47,118 @@ namespace TestAPP
         }
 
         [DllImport(CPPDLLPath, EntryPoint = "CPPMathLibrary_ManagedPort_GramSchmidt", CallingConvention = CallingConvention.Cdecl)]
-        private extern static double[][] _gramSchmidtOrthogonalization(double[][] matrix, int n);
+        private extern unsafe static double** _gramSchmidtOrthogonalization(double** matrix, int n);
 
         public static double[][] GramSchmidtOrthogonalization(double[][] matrix, int n)
         {
-            double[][] result = null;
-            try
-            {
-                result =  _gramSchmidtOrthogonalization(matrix, n);
+            double[][] m = new double[n][];
 
-            }
-            catch
+            unsafe
             {
-                //there needs to be a catch without a specified exception to catch the unmanaged code exception
+                fixed(double* row = &matrix[0][0])
+                {
+                    double*[] arrayofptr = new double*[matrix.Length];
+                    for (int i = 0; i < matrix.Length; i++)
+                    {
+                        fixed (double* ptr = &matrix[i][0])
+                        {
+                            arrayofptr[i] = ptr;
+                        }
+                    }
+
+                    fixed (double** ptrptr = &arrayofptr[0])
+                    {
+                        double** result = _gramSchmidtOrthogonalization(ptrptr, n);
+
+                        for(int i = 0; i < n; i++)
+                        {
+                            m[i] = new double[n];
+
+                            for(int j = 0; j < n; j++)
+                            {
+                                m[i][j] = result[i][j];
+                            }
+                        }
+
+                        ReleaseMemoryDoublePtrPtr(result, n);
+                    }
+                }
+            }
+
+            return m;
+        }
+
+        [DllImport(CPPDLLPath, EntryPoint = "CPPMathLibrary_ManagedPort_ReduceBasis_LLL", CallingConvention = CallingConvention.Cdecl)]
+        private extern unsafe static double** _reduceBasisLLL(double** matrix, int n, double alpha);
+
+        public static double[][] ReduceBasisLLL(double[][] matrix, int n, double alpha)
+        {
+            double[][] m = new double[n][];
+
+            unsafe
+            {
+                fixed (double* row = &matrix[0][0])
+                {
+                    double*[] arrayofptr = new double*[matrix.Length];
+                    for (int i = 0; i < matrix.Length; i++)
+                    {
+                        fixed (double* ptr = &matrix[i][0])
+                        {
+                            arrayofptr[i] = ptr;
+                        }
+                    }
+
+                    fixed (double** ptrptr = &arrayofptr[0])
+                    {
+                        double** result = _reduceBasisLLL(ptrptr, n, alpha);
+
+                        for (int i = 0; i < n; i++)
+                        {
+                            m[i] = new double[n];
+
+                            for (int j = 0; j < n; j++)
+                            {
+                                m[i][j] = result[i][j];
+                            }
+                        }
+
+                        ReleaseMemoryDoublePtrPtr(result, n);
+                    }
+                }
+            }
+
+            return m;
+        }
+
+        [DllImport(CPPDLLPath, EntryPoint = "CPPMathLibrary_ManagedPort_Simultaneous_SameDivisor", CallingConvention = CallingConvention.Cdecl)]
+        private extern unsafe static int** _simultaneous_SameDivisor(double* x, int n, double alpha, double epsilon);
+
+        public static int[][] SimultaneousSameDivisor(double[] x, int n, double alpha, double epsilon)
+        {
+            int[][] result = new int[n + 1][];
+
+            unsafe
+            {
+                fixed(double* arr = new double[n])
+                {
+                    for(int i = 0; i < n; i++)
+                    {
+                        arr[i] = x[i];
+                    }
+
+                    int n1 = n + 1;
+                    int** sd = _simultaneous_SameDivisor(arr, n, alpha, epsilon);
+                    for(int i = 0; i < n1; i++)
+                    {
+                        result[i] = new int[n1];
+                        for(int j = 0; j < n1; j++)
+                        {
+                            result[i][j] = sd[i][j];
+                        }
+                    }
+
+                    ReleaseMemoryIntPtrPtr(sd, n1);
+                }
             }
 
             return result;
--- TestAPP/Program.cs
+++ TestAPP/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
 
 namespace TestAPP
 {
@@ -7,15 +9,78 @@ namespace TestAPP
     {
         static void Main(string[] args)
         {
-            double num = Math.Sqrt(2);
+            int n = 4;
 
-            Console.WriteLine("Approximating sqrt(2)");
-            Console.WriteLine("Partial Quotients:");
-            int count = 10;
-            int[] result = CPPMathLibTest.ContinuedFraction(num, ref count);
-            CPPMathLibTest.PrintConvergents(result, count);
-            Console.WriteLine("Finished");
+            double[][] matrix = InitDoubleArr(n);
+            matrix[0][0] = -2;
+            matrix[0][1] = 7;
+            matrix[0][2] = 7;
+            matrix[0][3] = -5;
+            matrix[1][0] = 3;
+            matrix[1][1] = -2;
+            matrix[1][2] = 6;
+            matrix[1][3] = -1;
+            matrix[2][0] = 2;
+            matrix[2][1] = -8;
+            matrix[2][2] = -9;
+            matrix[2][3] = -7;
+            matrix[3][0] = 8;
+            matrix[3][1] = -9;
+            matrix[3][2] = 6;
+            matrix[3][3] = -4;
+
+            Console.WriteLine("Initial Array:");
+            PrintArrArr(matrix);
+
+            double[][] result = CPPMathLibTest.ReduceBasisLLL(matrix, n, 0.75);
+            Console.WriteLine("LLL Output:");
+            PrintArrArr(result, true);
+
             Console.ReadLine();
+        }
+
+        private static double[][] InitDoubleArr(int n, double value = 0)
+        {
+            double[][] arr = new double[n][];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = new double[n];
+                for (int j = 0; j < n; j++)
+                {
+                    arr[i][j] = value;
+                }
+            }
+            return arr;
+        }
+
+        private static void PrintArrArr<T>(T[][] arr, bool isnumeric = false)
+        {
+            var s = new StringBuilder();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr[i].Length; j++)
+                {
+                    if (isnumeric)
+                    {
+                        try
+                        {
+                            var t = string.Format("{0}   ", Convert.ToDouble(arr[i][j]).ToString("F5"));
+                            s.Append(t);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        s.AppendFormat("{0}\t", arr[i][j].ToString());
+                    }
+                }
+                s.AppendLine(string.Empty);
+            }
+            Console.WriteLine(s.ToString());
         }
     }
 }
