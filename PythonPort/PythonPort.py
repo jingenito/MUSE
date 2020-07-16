fccf045635bec8fe40cccd61062a5146e7e6dc52
Joe <jingenito@vcssoftware.com>
2020-07-16 13:22:43 -0400

    successfully made a gcd method call from python

--- PythonPort/PythonPort.py
+++ PythonPort/PythonPort.py
@@ -1,5 +1,22 @@
 import sys
-import clr
+from ctypes import cdll
+lib = cdll.LoadLibrary('c:/users/jinge/source/muse/cpplibrary/debug/cpplibrary.dll')
 
-dlldir = "C:\\dir\\of\\dll\\"
-dllname = "CDK_net"
+n1 = 0
+t1 = input("Enter a number ")
+if t1.isnumeric() :
+	n1 = int(t1)
+else :
+	print("Not number")
+	sys. exit()
+
+n2 = 0
+t2 = input("Enter a number ")
+if t2.isnumeric() :
+	n2 = int(t2)
+else :
+	print("Not Number")
+	sys. exit()
+
+gcd = lib.CPPMathLibrary_ManagedPort_GetGCD(n1, n2)
+print("The GCD is ", gcd)
