c1c333ca5338aa844370240eaf9bd62974ddccd8
Joe <jingenito@vcssoftware.com>
2020-07-16 22:54:22 -0400

    successfully added the continued fraction method to the python port

--- PythonPort/CPPLibrary/ContinuedFraction.py
+++ PythonPort/CPPLibrary/ContinuedFraction.py
@@ -1,8 +1,8 @@
 from CPPLibrary import *
 
-def ContinuedFractionExpansion(gamma : float, count : int) -> np.array :
+def ContinuedFractionExpansion(gamma : np.double, count : int) -> np.array :
     func = CPPMathLibrary.CPPMathLibrary_ManagedPort_ContinuedFraction
     func.argtypes = [ctypes.c_double, ctypes.c_int]
-    func.restype = np.ctypeslib.ndpointer(dtype=ctypes.c_int, shape=(count,))
+    func.restype = np.ctypeslib.ndpointer(dtype=ctypes.c_int, shape=(count,), flags="C_CONTIGUOUS")
     result = func(gamma, count)
     return np.array(result)
