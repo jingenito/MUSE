c635d9b8a740c18a3f072b9bae6f758029cdc6d8
Joe <jingenito@vcssoftware.com>
2020-07-16 23:18:59 -0400

    removed unnecessary code

--- PythonPort/CPPLibrary/ContinuedFraction.py
+++ PythonPort/CPPLibrary/ContinuedFraction.py
@@ -5,4 +5,10 @@ def ContinuedFractionExpansion(gamma : np.double, count : int) -> np.array :
     func.argtypes = [ctypes.c_double, ctypes.c_int]
     func.restype = np.ctypeslib.ndpointer(dtype=ctypes.c_int, shape=(count,), flags="C_CONTIGUOUS")
     result = func(gamma, count)
-    return np.array(result)
+    return result
+
+def PrintConvergents(qs : np.array, count : int) :
+    func = CPPMathLibrary.CPPMathLibrary_ManagedPort_PrintConvergents
+    func.argtypes = [np.ctypeslib.ndpointer(dtype=ctypes.c_int, shape=(count,), flags="C_CONTIGUOUS"), ctypes.c_int]
+    func.restype = None
+    func(qs, count)
