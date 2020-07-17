2e6acc47e6d303adc6c719f7a64189676a9e1b00
Joe <jingenito@vcssoftware.com>
2020-07-16 23:18:42 -0400

    cpplibrary module now looks for the dll in the same directory

--- PythonPort/CPPLibrary/__init__.py
+++ PythonPort/CPPLibrary/__init__.py
@@ -1,7 +1,11 @@
+import os, sys
 import numpy as np
 import ctypes
 
-CPPMathLibrary = ctypes.cdll.LoadLibrary('c:/users/jinge/source/muse/cpplibrary/debug/cpplibrary.dll')
+app_path = os.path.dirname(os.path.realpath('PythonPort.py'))
+dll_path = os.path.join(app_path, 'CPPLibrary', 'CPPLibrary.dll')
+
+CPPMathLibrary = ctypes.cdll.LoadLibrary(dll_path)
 
 def GreatestCommonDivisor(num1 : int, num2 : int) -> int :
     return CPPMathLibrary.CPPMathLibrary_ManagedPort_GetGCD(num1, num2)
