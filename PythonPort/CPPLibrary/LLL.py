11335ec7884d9af41c3372eff60550f2550398b4
Joe <jingenito@vcssoftware.com>
2020-07-17 21:29:12 -0400

    added a method to call LLL

--- PythonPort/CPPLibrary/LLL.py
+++ PythonPort/CPPLibrary/LLL.py
@@ -10,3 +10,14 @@ def GramSchmidtOrthogonalization(matrix: numpy.array) -> numpy.array :
     result = func(indata, count)
 
     return HelperFunctions.DblPtrPtrToNumpyArrArr(result, count)
+
+def ReduceBasis_LLL(matrix: numpy.array, alpha : numpy.double) -> numpy.array :
+    count = matrix.shape[0]
+    indata = HelperFunctions.NumpyArrArrToDblPtrPtr(matrix)
+
+    func = CPPMathLibrary.CPPMathLibrary_ManagedPort_ReduceBasis_LLL
+    func.argtypes = [ctypes.POINTER(ctypes.POINTER(ctypes.c_double)), ctypes.c_int32, ctypes.c_double]
+    func.restype = ctypes.POINTER(ctypes.POINTER(ctypes.c_double))
+    result = func(indata, count, alpha)
+
+    return HelperFunctions.DblPtrPtrToNumpyArrArr(result, count)
--- PythonPort/PythonPort.py
+++ PythonPort/PythonPort.py
@@ -2,15 +2,18 @@ import sys
 import numpy
 from CPPLibrary import LLL
 
-x = numpy.array([[3.0, -1.0, 5.0], 
-                [-5.0, 2.0, -1.0], 
-                [-3.0, 9.0, 2.0]], 
+alpha = 0.75
+x = numpy.array([[-2, 7, 7, -5], 
+                [3, -2, 6 , -1], 
+                [2, -8, -9, -7],
+                [8, -9, 6, -4]], 
                 dtype = numpy.double)
-gso = LLL.GramSchmidtOrthogonalization(x)
+gso = LLL.ReduceBasis_LLL(x, alpha)
 
+print("Alpha: ", alpha)
 print("X:")
 print(x)
 print()
-print("GSO:")
+print("LLL:")
 print(gso)
 
