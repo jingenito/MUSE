1e26858634be70811d7c6b509adf6f0dd7dd50f7
Joe <jingenito@vcssoftware.com>
2020-07-20 12:50:33 -0400

    added json serialization to the python port

--- PythonPort/PythonPort.py
+++ PythonPort/PythonPort.py
@@ -1,18 +1,12 @@
 import sys
 import numpy
-from CPPLibrary import SimultaneousDiophantine
+from Util import JSONSerializer
 
-alpha = 0.75
-epsilon = 1.0 / 10
-x = numpy.array([239.0 / 169.0, 265.0 / 153.0, 682.0 / 305.0, 590.0 / 223.0 ], 
-                dtype = numpy.double)
-sd = SimultaneousDiophantine.SameDivisor(x, alpha, epsilon)
+filename = "C:\\Users\\jinge\\source\\MUSE\\CPP\\Debug\\test.json"
 
-print("Alpha: ", alpha)
-print("Epsilon: ", epsilon)
-print("X:")
-print(x)
-print()
-print("LLL C:")
-print(sd)
+json = JSONSerializer.DeserializeJSON(filename)
+
+print('Count: ', json['Count'])
+print('Op Count: ', json['OpCount'])
+print('q Sequence: ', json['qSequence'])
 
---
+++ PythonPort/Util/__init__.py
@@ -0,0 +1,1 @@
+# making Util a module
